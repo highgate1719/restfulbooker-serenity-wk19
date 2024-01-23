64455220433c6b8eebc59b77c3636e3c6a7e71a5
highgate1719 <highgate1719@gmail.com>
2024-01-23 20:41:48 +0000

    RestfulBooker serenity WK19

---
+++ src/test/java/restful/booker/testbase/TestBase.java
@@ -0,0 +1,18 @@
+package restful.booker.testbase;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+import restful.booker.utils.PropertyReader;
+
+public class TestBase {
+
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseURI");
+
+    }
+
+}
